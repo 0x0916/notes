198acd5f1482c584773d909fef1f868ba681a343
0x0916 <w@laoqinren.net>
2017-05-07 13:32:33 +0800

    update api.sh
    Signed-off-by: 0x0916 <w@laoqinren.net>

--- code/c/seccomp-api/api.sh
+++ code/c/seccomp-api/api.sh
@@ -4,11 +4,20 @@ Usage:./dropper <syscall_nr> <arch> <errno> <prog> [<args>]
 Hint for <arch>: AUDIT_ARCH_I386: 0x40000003
 		 AUTIT_ARCH_X86_64: 0xC000003E
 $  # 我们使用89这个错误代码
+$ errno 89
+EDESTADDRREQ 89 Destination address required
+$  # 我们禁用掉函数execve
+$ scmp_sys_resolver -a $(uname -m) -t execve
+59
 $ ./dropper 59 0xC000003E 89 /usr/bin/whoami
 execv: Destination address required
 $ # 禁用系统调用write
+$ scmp_sys_resolver -a $(uname -m) -t write
+1
 $ ./dropper 1 0xC000003E 89 /usr/bin/whoami
 $ # 禁用一个whoami不用的系统调用
+$ scmp_sys_resolver -a $(uname -m) -t preadv
+295
 $ ./dropper 295 0xC000003E 89 /usr/bin/whoami
 root
 
